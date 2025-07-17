16978f277a90ca0493cc4b28964ea71af88de1d6
Guilherme <guilhermefogacamessias@alunos.utfpr.edu.br>
2025-07-17 15:14:42 -0300

    add: adicionando rota de cadastro e criptografando senha

--- routes/public.js
+++ routes/public.js
@@ -1,10 +1,32 @@
 import express from 'express'
+import { PrismaClient } from '@prisma/client'
+import bcrypt from 'bcrypt'
 
-const router= express.Router()
+const router = express.Router()
+const prisma = new PrismaClient()
+router.post('/cadastro', async(req, res) => {
 
-router.post('/cadastro',(req,res) => { 
-    const user = req.body
-    res.status(201).json(user)
+    try {
+        const user = req.body
+
+        const salt = await bcrypt.genSalt(10)
+        const hashPassword = await bcrypt.hash(user.password,salt)
+
+        const userDB = await prisma.user.create({
+            data: {
+                email: user.email,
+                name: user.name,
+                password:hashPassword,
+        }
+        })
+        res.status(201).json(userDB)
+    } catch (err) {
+        res.status(500).json({ message: "Erro no servidor, tente novamente mais tarde" })
+    }
+
 })
+
+router.get('/login', async(req, res) => {})
+
 
 export default router
